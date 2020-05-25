a2553fee9dc2fc2bac9763690590dfb2e6187330
= <joao.campos@osf-global.com>
2020-05-25 09:42:26 -0300

    adiciona bootstrap 4 grid

--- src/section.js
+++ src/section.js
@@ -4,4 +4,31 @@ midSection.innerHTML = 'midSection'
 
 document.body.appendChild(midSection)
 
+const btn = document.createElement('button')
+btn.className = 'btn'
+btn.innerHTML = 'click me'
+
+midSection.appendChild(btn)
+
+const rowsWrapper = document.createElement('div')
+
+const rows = `<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>`
+
+btn.addEventListener('click', () => {
+    console.log('clicked')
+    rowsWrapper.innerHTML = rows
+})
+
 export default midSection
